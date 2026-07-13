6464779febf3906c292551adf0a9609bef30f91e
Ankan <ankansecured2008@gmail.com>
2026-07-13 22:52:05 +0530

    ODD EVEN INDEX SEPERATE ADDED

---
+++ C++programming/LINKED LIST/oddevenll.cpp
@@ -0,0 +1,65 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+class Node
+{
+    public:
+       int data;
+       Node* next;
+       Node(int data1,Node* next1)
+       {
+           data=data1;
+           next=next1;
+       }
+       Node(int data1)
+       {
+           data=data1;
+           next=nullptr;
+       }
+};
+Node* convert(vector<int>&arr)
+{
+    Node* head=new Node(arr[0]);
+    Node* mover=head;
+    for(int i=1;i<arr.size();i++)
+    {
+        Node* temp=new Node(arr[i]);
+        mover->next=temp;
+        mover=temp;
+    }
+    return head;
+}
+void print(Node* head)
+{
+    while(head!=NULL)
+    {
+        cout<<head->data<<" ";
+        head=head->next;
+    }
+    cout<<endl;
+}
+int main()
+{
+    int n;
+    cin>>n;
+    int a[n];
+    vector<int>v;
+    for(int i=0;i<n;i++)
+    {
+        cin>>a[i];
+        v.push_back(a[i]);
+    }
+    Node* head=convert(v);
+    Node* odd=head;
+    Node* even=head->next;
+    Node* evenhead=even;
+    while(even!=NULL && even->next!=NULL)
+    {
+        odd->next=odd->next->next;
+        even->next=even->next->next;
+        odd=odd->next;
+        even=even->next;
+    }
+    odd->next=evenhead;
+    print(head);
+}
