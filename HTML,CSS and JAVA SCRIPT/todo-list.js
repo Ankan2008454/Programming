b6ea591ecd362ec5dfc08a68f5cfbf9b45c178a6
Ankan2008454 <ankansecured2008@gmail.com>
2026-08-02 00:48:37 +0530

    todolist added

---
+++ HTML,CSS and JAVA SCRIPT/todo-list.js
@@ -0,0 +1,36 @@
+const todoList=[{
+    name:'make dinner',
+    dueDate:'2022-12-22'
+},
+{
+    name:'wash dishes',
+    dueDate:'2022-12-22'
+}];
+
+function renderTodoList()
+{
+    let todoListHTML='';
+    for(let i=0;i<todoList.length;i++)
+    {
+        const todoObject=todoList[i];
+        const name=todoObject.name;
+        const dueDate=todoObject.dueDate;
+        const html=`<p>${name} ${dueDate} <button onclick="
+        todoList.splice(${i},1);
+        renderTodoList();
+        ">Delete</button> </p>`;
+        todoListHTML+=html;
+    }
+    console.log(todoListHTML);
+    document.querySelector('.js-todo-list').innerHTML=todoListHTML;
+}
+function addTodo()
+{
+    const inputElement=document.querySelector('.js-input');
+    const name=inputElement.value;
+    const dateInputElement=document.querySelector('.js-date');
+    const dueDate=dateInputElement.value;
+    todoList.push({name: name,dueDate: dueDate});
+    console.log(todoList);
+    renderTodoList();
+}
