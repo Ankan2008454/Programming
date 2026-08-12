7904b9e9f484086d8e6efed00e6355b55e3c70dc
Ankan2008454 <ankansecured2008@gmail.com>
2026-08-12 19:08:42 +0530

    amazon project continued

---
+++ HTML,CSS and JAVA SCRIPT/Amazon-project/data/cart-oop.js
@@ -0,0 +1,91 @@
+const cart = {
+    cartItems: undefined,
+
+    localStorage: function() {
+        this.cartItems = JSON.parse(localStorage.getItem('cart-oop'));
+
+        if (!this.cartItems) {
+            this.cartItems = [
+                {
+                    productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+                    quantity: 2,
+                    deliveryOptionId: '1'
+                },
+                {
+                    productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
+                    quantity: 1,
+                    deliveryOptionId: '2'
+                }
+            ];
+
+            this.saveToStorage();
+        }
+    },
+
+    saveToStorage: function() {
+        localStorage.setItem(
+            'cart-oop',
+            JSON.stringify(this.cartItems)
+        );
+    },
+
+    addToCart(productId) {
+        let matchingItem;
+
+        this.cartItems.forEach((item) => {
+            if (productId === item.productId) {
+                matchingItem = item;
+            }
+        });
+
+        if (matchingItem) {
+            matchingItem.quantity += 1;
+        } else {
+            cart.cartItems.push({
+                productId: productId,
+                quantity: 1,
+                deliveryOptionId: '1'
+            });
+        }
+
+        this.saveToStorage();
+    },
+
+    removeFromCart(productId) {
+        const newCart = [];
+
+        this.cartItems.forEach((cartItem) => {
+            if (cartItem.productId !== productId) {
+                newCart.push(cartItem);
+            }
+        });
+
+        this.cartItems = newCart;
+        this.saveToStorage();
+    },
+
+    updateDeliveryOption(productId, deliveryOptionId) {
+        let matchingItem;
+
+        this.cartItems.forEach((item) => {
+            if (productId === item.productId) {
+                matchingItem = item;
+            }
+        });
+
+        if (matchingItem) {
+            matchingItem.deliveryOptionId = deliveryOptionId;
+            saveToStorage();
+        }
+    }
+};
+cart.localStorage();
+cart.addToCart('83d4ca15-0f35-48f5-b7a3-1ea210004f2e');
+console.log(cart);
+
+
+
+
+
+
+
--- HTML,CSS and JAVA SCRIPT/Amazon-project/scripts/checkout.js
+++ HTML,CSS and JAVA SCRIPT/Amazon-project/scripts/checkout.js
@@ -1,4 +1,5 @@
 import { renderOrderSummary } from './checkout/order-summary.js';
 import {renderPaymentSummary} from './checkout/payment-summary.js';
+import '../data/cart-oop.js';
 renderOrderSummary();
 renderPaymentSummary();
