1c1e1378659a8fcbd956a36ac0a4505af4ac0ba6
Ankan2008454 <ankansecured2008@gmail.com>
2026-08-14 20:38:05 +0530

    EXTRA INFO ADDED

--- HTML,CSS and JAVA SCRIPT/Amazon-project/data/products.js
+++ HTML,CSS and JAVA SCRIPT/Amazon-project/data/products.js
@@ -22,7 +22,28 @@ class Product
   {
    return `$${formatCurrency(this.priceCents)}`;
   }
+  extraInfoHTML()
+  {
+    return'';
+  }
 }
+class Clothing extends Product
+{
+  sizeChartLink;
+  constructor(productDetails)
+  {
+    super(productDetails);
+    this.sizeChartLink=productDetails.sizeChartLink;
+  }
+  extraInfoHTML()
+  {
+    //super.extraInfoHTML();
+    return `
+     <a href="${this.sizeChartLink}" target="_blank">Size Chart</a>
+    `;
+  }
+}
+
 const product1=new Product({
     id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
     image: "images/products/athletic-cotton-socks-6-pairs.jpg",
@@ -711,6 +732,10 @@ export const products = [
     ]
   }
 ].map((productDetails)=>{
+  if(productDetails.type==='clothing')
+  {
+    return new Clothing(productDetails)
+  }
   return new Product(productDetails);
 });
 console.log(products);
