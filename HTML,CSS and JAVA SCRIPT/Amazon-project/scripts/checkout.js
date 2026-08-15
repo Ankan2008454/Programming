96fab1dec92159f38df7b0c402301eab437b9c47
Ankan2008454 <ankansecured2008@gmail.com>
2026-08-15 16:37:24 +0530

    add backend practice file

--- HTML,CSS and JAVA SCRIPT/Amazon-project/scripts/checkout.js
+++ HTML,CSS and JAVA SCRIPT/Amazon-project/scripts/checkout.js
@@ -1,5 +1,6 @@
 import { renderOrderSummary } from './checkout/order-summary.js';
 import {renderPaymentSummary} from './checkout/payment-summary.js';
-import '../data/cart-oop.js';
+//import '../data/cart-oop.js';
+import '../data/backend-practice.js';
 renderOrderSummary();
 renderPaymentSummary();
